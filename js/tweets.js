db1459f85bd4c94e386ef4bb8a3025d93c9c7284
Miles Gilbert <miles.gilbert@t-3.com>
2017-06-13 13:19:33 -0500

    added comments to explain what I'm trying to do (not working)

--- js/tweets.js
+++ js/tweets.js
@@ -1,3 +1,5 @@
+var $recentTweet;
+
 $(function(){
 
 	$.ajax({
@@ -9,16 +11,20 @@ $(function(){
 				
 				var $tweets = $('<ul></ul>');
 				$.each(response, function(i, obj) {
-					// if (obj.quoted_status != null) {
-					// 	if(obj.quoted_status.text == null) {
-					// 		$tweets.append('<li>KILL</li>');
-					// 	} else {
-					// 		$tweets.append('<li>' + obj.quoted_status.text + '</li>');
-					// 	}
-					// }
-					if (obj.quoted_status != null) {
-						$tweets.append('<li>' + obj.quoted_status.id + '</li>');
+
+					// this SHOULD look at all of my tweets to see if they are:
+						// 1. Quoted tweets
+						// 2. Have the "quoted_status" key in the JSON object
+
+						// When someone deletes a tweet that you quoted, your tweet retains the "is_quoted_status" key (with value "true") but loses the "quoted_status" key
+
+					if (obj.is_quoted_status == true && obj.hasOwnProperty('quoted_status') == false) {
+						$tweets.append('<li>KILL</li>'); //Will eventually want to count these for the total, but for now, I want to see a list to make sure its getting all of them
 					}
+					
+					// Uncomment below to show all tweets regardless of "quoted_status" being present
+					// $tweets.append('<li>' + obj + '</li>');
+
 				});
 
 				$('.tweets-container').html($tweets);
---
+++ one_tweet.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once('twitter_proxy.php');
+
+// Twitter OAuth Config options
+$oauth_access_token = '49634023-JNqy4RZW8FDCz6KG5xfuvameA3GohRF1LkvymqZJN';
+$oauth_access_token_secret = 'yptLQTzS4mUi0JAJzHud0lrrMUgsGGJQLJaAGxSrFIlIY';
+$consumer_key = 'PAWJh6RMzWwc2BEocbsoOhuBI';
+$consumer_secret = 'YMK6MuhOJLQ4ppm9Q0HVREFBc3b0qXV3NYpI7nm0zE4gChszW0';
+$user_id = '49634023';
+$screen_name = '_brainnews';
+$count = 1;
+
+$twitter_url = 'statuses/user_timeline.json';
+$twitter_url .= '?user_id=' . $user_id;
+$twitter_url .= '&screen_name=' . $screen_name;
+$twitter_url .= '&count=' . $count;
+
+// Create a Twitter Proxy object from our twitter_proxy.php class
+$twitter_proxy = new TwitterProxy(
+	$oauth_access_token,			// 'Access token' on https://apps.twitter.com
+	$oauth_access_token_secret,		// 'Access token secret' on https://apps.twitter.com
+	$consumer_key,					// 'API key' on https://apps.twitter.com
+	$consumer_secret,				// 'API secret' on https://apps.twitter.com
+	$user_id,						// User id (http://gettwitterid.com/)
+	$screen_name,					// Twitter handle
+	$count							// The number of tweets to pull out
+);
+
+// Invoke the get method to retrieve results via a cURL request
+$tweets = $twitter_proxy->get($twitter_url);
+
+echo $tweets;
+
+?>
